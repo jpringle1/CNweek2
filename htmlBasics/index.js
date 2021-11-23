d9af39340f7456fa873617517d5424484065c209
jpringle1 <joe-pringle@outlook.com>
2021-11-23 09:55:50 +0000

    day 1 updates

---
+++ htmlBasics/index.js
@@ -0,0 +1,18 @@
+const shoppingList = [
+    list1 = ["list 1 - item 1",
+    "list 1 - item 2",
+    "list 1 - item 3"
+    ],
+
+    list2 = ["list 2 - item 1",
+    "list 2 - item 2",
+    "list 2 - item 3"
+    ],
+
+    list3 = ["list 3 - item 1",
+    "list 3 - item 2",
+    "list 3 - item 3"
+    ]
+]
+
+console.log(shoppingList[1][2])
